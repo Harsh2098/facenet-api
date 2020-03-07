74cddcb9c90e71d564698545a8ab341e143136a3
Harsh Mahajan <harshmahajan927@yahoo.com>
2020-03-07 12:31:53 +0530

    Update README.md

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ server.listen(port, () => {
   console.log("\nRoutes available:\n-----------------\n");
   console.log("(POST) /api/student\n(GET)  /api/student\n");
   console.log(
-    "(POST) /api/upload/image\n\n(GET)  /api/train\n\n(POST) /api/identify"
+    "(POST) /api/upload/image\n\n(GET)  /api/train\n\n(POST) /api/identify\n"
   );
 });
 
